a5c18b6a279dc775f32bbab4e0902e16c741ee8f
mgmeti <mgmeti18@gmail.com>
2024-10-11 16:18:41 +0530

    Added JsonIgnore annotation and used postman to test api

--- src/main/java/com/datababa/namma_shops/model/Category.java
+++ src/main/java/com/datababa/namma_shops/model/Category.java
@@ -1,5 +1,6 @@
 package com.datababa.namma_shops.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +20,7 @@ public class Category {
 
     private String name;
 
+    @JsonIgnore
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 
